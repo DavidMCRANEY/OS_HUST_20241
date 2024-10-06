f85165d8f9578fbf0e5152d7a24fcc32b96a5049
nqv96 <vuong09062002@gmail.com>
2024-10-06 11:57:05 +0700

    add while loop

--- rcnn.py
+++ rcnn.py
@@ -1,8 +1,11 @@
-print('Test push code again')
+print("Test push code again")
 for i in range(5):
-    print(i, end=' ')
+    print(i, end=" ")
 
 arr = [1, 2, 3, 4, 5]
 for i in range(len(arr)):
     print(arr[i])
-    
+i = 1
+while i < 5:
+    print("I vs You")
+    i += 1
