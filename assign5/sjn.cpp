7f4c86a6594df3d3653ebdd970985c25988fee16
nqv96 <vuong09062002@gmail.com>
2024-10-27 22:47:01 +0700

    add hw

---
+++ assign5/sjn.cpp
@@ -0,0 +1,10 @@
+// sjn standard for 'shortest job first' or 'shortest job next' (non preemptive)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    cout << "Vuong";
+    cout << endl;
+    return 0;
+}
